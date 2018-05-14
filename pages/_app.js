82db7b05c2a20dc0dbc7b0ded7a4e9c6888888f8
Dominic Farquharson <farquharsondominic@gmail.com>
2018-05-14 09:56:27 -0400

    add custom app component

---
+++ pages/_app.js
@@ -0,0 +1,32 @@
+import App, { Container } from 'next/app';
+import React from 'react';
+import Link from 'next/link';
+
+export default class MyApp extends App {
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <Container>
+        <div>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+          <Link href="/search">
+            <a>Search</a>
+          </Link>
+        </div>
+        <Component {...pageProps} />
+      </Container>
+    );
+  }
+}
